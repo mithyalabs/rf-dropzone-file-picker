4c6fa8b022179048eef2907ee58143ebe3da0d85
aniganesh <aniganesh741@gmail.com>
2020-10-06 01:29:34 +0530

    Improved component, added documentation.

--- README.md
+++ README.md
@@ -1,14 +1,40 @@
+**Table of Contents**
+
+- [rf-dropzone-file-picker](#rf-dropzone-file-picker)
+	- [Dependencies](#dependencies)
+	- [Installation](#installation)
+	- [How to use](#how-to-use)
+
 # rf-dropzone-file-picker
+
 A package that runs on top of [react-forms](https://github.com/mithyalabs/ml-react-forms) and provides a drag and drop file input.
-## Dependencies:
+
+## Dependencies
+
 1. react-dropzone
 2. react-forms
 3. clsx
 4. lodash
 5. formik
-## How to use:
-In your config, add the following to get the basic dropzone file input:
+
+## Installation
+
+``` 
+npm i https://github.com/mithyalabs/rf-dropzone-file-picker 
 ```
+
+**dependencies installation** 
+
+``` 
+npm i https://github.com/mithyalabs/react-forms 
+npm i react-dropzone clsx lodash formik
+```
+
+## How to use
+
+In your config, add the following to get the basic dropzone file input:
+
+``` 
 {
 	type: 'drop-file',
 	valueKey: 'myValueKey',
@@ -16,19 +42,30 @@ In your config, add the following to get the basic dropzone file input:
 }
 
 ```
+
 fieldProps has the following type:
-```
+
+``` 
 {
-	onDropFile?: (files: File[]) => void
-	/* onDropFile is triggered soon after a file is dropped in the dropzone and has been added to the form */
+	onDropFile?: (imgFiles: TFile[], remFiles?: File[]) => void
+	loadFile?: (files: File[]) => Promise<File[]>
+	readAs?: ReadAsType
 	multiple?: boolean
 	label?: string | JSX.Element
-	accept?: string, 
-	readAs?: ReadAsType
+	accept?: string
+	encoding?: string
 	activeClass?: string
 	defaultClass?: string
 	fullWidth?: boolean
+	renderAccepted?: (files: File[]) => JSX.Element
 }
 ```
-You can customise the look of the dropzone using activeClass and defaultClass props. Active class contains rules that will take effect on dragging a file over the area.Eg.: backgroundColor, textColor, etc., while defaultClass is for class with rules that will not be affected by dragging a file over the area. Eg.: height, width, border, borderRadius, etc.
-fullWidth sets the width to 100%. Default is 400px.
+
+**Notes:**
+
+* You can customise the look of the dropzone using activeClass and defaultClass props. Active class contains rules that will take effect on dragging a file over the area. Eg.: backgroundColor, textColor, etc., while defaultClass is for class with rules that will not be affected by dragging a file over the area. Eg.: height, width, border, borderRadius, etc.
+
+* fullWidth sets the width to 100%. Default is 400px.
+
+* The renderAccepted function should take an array of files and display them within a container anyway you choose.
+* By default only the file names are displayed with a dashed border and in a container with minWidth 200 on the right side of the dropzone.
--- src/lib/MUIDropFile.tsx
+++ src/lib/MUIDropFile.tsx
@@ -4,22 +4,27 @@ import clsx from 'clsx';
 import { FormikValues } from 'formik';
 import React from 'react';
 import { useDropzone } from 'react-dropzone'
-import { IFieldProps, IMUIFileInputProps, MUIFileInput, ReadAsType, setValue } from 'react-forms'
+import { IFieldProps, IMUIFileInputProps, MUIFileInput, TFile, ReadAsType, setValue, processFilesWithCallback } from 'react-forms'
+
 
 
 export interface DropFileFieldProps {
-	onDropFile?: (files: File[]) => void
-	multiple?: boolean
-	activeClass?: string
-	label?: string | JSX.Element
-	accept?: string,
+	onDropFile?: (imgFiles: TFile[], remFiles?: File[]) => void
+	loadFile?: (files: File[]) => Promise<File[]>
 	readAs?: ReadAsType
+	multiple?: boolean
+	label?: string | JSX.Element
+	accept?: string
+	encoding?: string
+	activeClass?: string
+	defaultClass?: string
 	/* 
-	Active class contains rules that will take effect on dragging a file over the area.Eg.: backgroundColor, textColor, etc.,
+	Active class contain rules that will take effect on dragging a file over the area.Eg.: backgroundColor, textColor, etc.,
 	defaultClass is for class with rules that will not be affected by dragging a file over the area. Eg.: height, width, border, borderRadius, etc.
 	 */
-	defaultClass?: string
 	fullWidth?: boolean
+	acceptedFileArea?: string
+	renderAccepted?: (files: File[]) => JSX.Element
 }
 export interface DropFileProps extends IFieldProps {
 	fieldProps?: DropFileFieldProps
@@ -28,6 +33,13 @@ export interface DropFileProps extends IFieldProps {
 export const MUIDropFile: React.FC<DropFileProps> = (props: DropFileProps) => {
 	const { fieldProps = {} as DropFileFieldProps, formikProps = {} as FormikValues } = props
 	const classes = useStyles(fieldProps)
+	const [files, setFiles] = React.useState<File[]>([])
+	const handleDisplay = (files: File[]) => {
+		console.log(files)
+		return <Box display="flex" flexDirection='column' alignItems="center" justifyContent="center">{files.map((file: File, index: number) =>
+			<Box className={classes.acceptedFile} m={1} p={2} key={file.name + index}>{file.name}</Box>)}</Box>
+	}
+
 	const {
 		accept,
 		onDropFile,
@@ -35,35 +47,58 @@ export const MUIDropFile: React.FC<DropFileProps> = (props: DropFileProps) => {
 		defaultClass = classes.defaultClass,
 		activeClass = classes.activeClass,
 		label = "Drag and drop a file/files here",
-		readAs, ...rest
+		readAs,
+		loadFile,
+		encoding = 'utf-8',
+		renderAccepted = handleDisplay,
+		fullWidth = true,
+		acceptedFileArea,
+		...rest
 	} = fieldProps
 	const wrapWith = (input: JSX.Element) => (
-		<Box {...getRootProps()} className={clsx(defaultClass, isDragActive ? activeClass : "")}
-			display="flex" alignItems="center" justifyContent="center" >
-			<Typography>{label}</Typography>
-			{input}
+		<Box display="flex" alignItems="center" justifyContent="center" minWidth={700} width={fullWidth ? '100%' : 600}>
+			<Box m={2} {...getRootProps()} className={clsx(defaultClass, isDragActive ? activeClass : "")}
+				display="flex" alignItems="center" justifyContent="center" >
+				<Typography>{label}</Typography>
+				{input}
+			</Box>
+			<Box width={300} className={acceptedFileArea}>
+				{renderAccepted(files)}
+			</Box>
 		</Box>
 	)
-	const handleDrop = (files: File[]) => {
+	const handleDrop = async (files: File[]) => {
+		if (loadFile) {
+			const res = await loadFile?.(files)
+			setFiles(res)
+			setValue(res, formikProps, fieldProps)
+			return
+		}
 		setValue(files, formikProps, fieldProps);
-		onDropFile?.(files)
+		processFilesWithCallback(files, (prop: { imgs: TFile[], rem: any[] }) => {
+			const { imgs, rem } = prop
+			onDropFile?.(imgs, rem)
+			setFiles(rem.concat(imgs as unknown as File[]))
+		}, readAs, encoding)
+
 	}
+
+
 	const onDrop = React.useCallback(handleDrop, [])
 	const { isDragActive, getRootProps, getInputProps } = useDropzone({ onDrop })
 
-	return (<>
+	return (
 		<MUIFileInput fieldProps={{ ...rest, multiple, wrapWith, accept, readAs, nativeInputProps: { ...getInputProps() } } as IMUIFileInputProps} formikProps={formikProps} />
-	</>
 	)
 }
+
 
 const useStyles = makeStyles<Theme, Pick<DropFileFieldProps, 'fullWidth'>>((theme) => {
 	return (createStyles({
 		defaultClass: {
-			border: '1px dashed grey', borderRadius: 8, minWidth: 400, height: 300, background: 'lightgrey', position: 'relative',
-			margin: theme.spacing(2),
-			width: ({ fullWidth }) => fullWidth ? '100%' : 400
+			border: '1px dashed grey', borderRadius: 8, minWidth: 400, height: 300, background: 'lightgrey', position: 'relative', flex: 1,
 		},
-		activeClass: { backgroundColor: 'transparent' }
+		activeClass: { backgroundColor: 'transparent' },
+		acceptedFile: { background: 'lightgrey', border: '1px dashed grey', margin: theme.spacing(1), padding: theme.spacing(1), minWidth: 200 }
 	}))
 })
